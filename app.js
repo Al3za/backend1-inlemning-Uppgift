9cfd47c06819f9b31c87b3888969ec6488fb525d
Your Name <akecakabro@gmail.com>
2022-03-06 11:52:34 +0100

    Pic_upload

--- app.js
+++ app.js
@@ -4,7 +4,6 @@ const path=require('path');
 const app = express()
 const PORT = 3000;
 
-
 app.use(express.urlencoded({extended:true}));
 const mongoose=require('mongoose');
 mongoose.connect('mongodb://localhost/SignIn');
@@ -13,7 +12,18 @@ const passport=require('passport');
 const session=require('express-session');
 const { errors } = require("passport-local-mongoose");
 
+app.use(express.static('./public'))
+
 passport.use(USERS.createStrategy());
+
+const storage=multer.diskStorage({
+  destination:'./public/uploads',
+  filename: function(req,file,cb){
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+  }
+});
+
+var upload=multer({storage:storage});
 
 app.use(session({
   secret:'2228983928',
@@ -34,11 +44,16 @@ app.get('/',(req,res)=>{
   }
 })
 
-app.post('/emailName',(req,res)=>{
-  const {email,fullname}=req.body;
-  console.log(req.body);
-  res.render('userProfil.ejs',{info:[req.body]})
-})
+app.post('/upload',upload.single('image'),(req,res,next)=>{
+    const {email,fullname}=req.body;
+   const {file:filename}=req.file; 
+
+  res.render('userProfil.ejs',{
+    infoName:req.body.fullname,
+    infoMail:req.body.email,
+    photo:`uploads/${req.file.filename}`
+  })
+  })
 
 app.get('/login',(req,res)=>{
   res.render('login.ejs')
@@ -67,5 +82,3 @@ app.post('/signin',async (req,res)=>{
 app.listen(PORT, () => {
   console.log(`Started Express server on port ${PORT}`);
 });
-
-console.log('check github');
